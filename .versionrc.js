2b415226397ad818fd2d0aa3d4befb77641f8bbd
laqieer <laqieer@126.com>
2021-06-29 15:11:37 +0800

    chore: use standard-version

---
+++ .versionrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+    "types": [
+        {"type": "feat", "section": "Features"},
+        {"type": "fix", "section": "Bug Fixes"},
+        {"type": "chore", "hidden": true},
+        {"type": "docs", "hidden": true},
+        {"type": "style", "hidden": true},
+        {"type": "refactor", "hidden": true},
+        {"type": "perf", "hidden": true},
+        {"type": "test", "hidden": true},
+        {"type": "revert", "hidden": true}
+    ],
+    "packageFiles": [
+        {
+            "filename": "VERSION",
+            "type": "plain-text"
+        }
+    ],
+    "bumpFiles": [
+        {
+            "filename": "VERSION",
+            "type": "plain-text"
+        }
+    ]
+}
