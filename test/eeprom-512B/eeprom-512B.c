5a90187403fa33b2530ed8c18f6fa31bb13d36d2
laqieer <laqieer@126.com>
2021-06-30 18:16:06 +0800

    test: test backup media support for 512B EEPROM

---
+++ test/eeprom-512B/eeprom-512B.c
@@ -0,0 +1,52 @@
+#include <tonc.h>
+
+#include <gba_eeprom.h>
+
+#define BUFFER_SIZE 4
+
+int main(void) {
+    int err;
+	u16 buffer[BUFFER_SIZE];
+
+	REG_DISPCNT = DCNT_MODE0 | DCNT_BG0;
+	
+	tte_init_chr4c_default(0, BG_CBB(0) | BG_SBB(31));
+
+    // Write Test
+    err = eeprom_512_write(0, "EEPROM_Vnnn\n");
+    if (err) {
+        tte_write("EEPROM(512B) Write Failed!");
+        goto end;
+    }
+
+    err = eeprom_512_write(1, "nnn\n");
+    if (err) {
+        tte_write("EEPROM(512B) Write Failed!");
+        goto end;
+    }
+
+    // Read Test
+    err = eeprom_512_read(0, buffer);
+    if (err) {
+        tte_write("EEPROM(512B) Read Failed!");
+    }
+    tte_write(buffer);
+
+    err = eeprom_512_read(1, buffer);
+    if (err) {
+        tte_write("EEPROM(512B) Read Failed!");
+    }
+    tte_write(buffer);
+
+    // Final Result
+    tte_write("EEPROM(512B) Test Passed!");
+
+end:
+	irq_init(NULL);
+	irq_enable(II_VBLANK);
+
+	while (1) {
+		VBlankIntrWait();
+	}
+	
+}
