9e21e59eaef13059e55fce8d11c9d55fce0b8396
laqieer <laqieer@126.com>
2021-07-05 21:13:30 +0800

    feat: add support to show error codes and error messages
    add `SavErrCodes[ErrorCode]` and `SavErrMsgs[ErrorCode]`

--- include/err_def.h
+++ include/err_def.h
@@ -10,4 +10,18 @@ enum ErrorCode {
     E_TIMEOUT, ///< Operation timeout.
 };
 
+/**
+ * @brief Error codes.
+ *
+ * Use `SavErrCodes[ErrorCode]` to get error code names.
+ */
+extern const char * const SavErrCodes[];
+
+/**
+ * @brief Error messages.
+ *
+ * Use `SavErrMsgs[ErrorCode]` to get error code messages.
+ */
+extern const char * const SavErrMsgs[];
+
 #endif /* ERR_DEF_H */
---
+++ src/err_def.c
@@ -0,0 +1,17 @@
+const char * const SavErrCodes[] = {
+    "E_SUCCESS",
+    "E_INVALID_PARAM",
+    "E_OUT_OF_RANGE",
+    "E_VERIFY_FAIL",
+    "E_UNSUPPORTED_DEVICE",
+    "E_TIMEOUT",
+};
+
+const char * const SavErrMsgs[] = {
+    "No error",
+    "Invalid input parameter",
+    "Address is out of range",
+    "Failed to verify written data",
+    "Device type is not supported",
+    "Operation timeout",
+};
